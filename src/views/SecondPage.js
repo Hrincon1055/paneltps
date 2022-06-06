d6ce2eedeba8f3c33c973b98be97492aaded804a
desarrollo.tercerosTPS <mariogithub@outlook.com>
2022-06-06 10:45:48 -0500

    prueba de commit

--- src/views/SecondPage.js
+++ src/views/SecondPage.js
@@ -7,7 +7,7 @@ const SecondPage = () => {
         <CardTitle>Create Awesome 🙌</CardTitle>
       </CardHeader>
       <CardBody>
-        <CardText>This is your second page.</CardText>
+        <CardText>This is your second page prueba</CardText>
         <CardText>
           Chocolate sesame snaps pie carrot cake pastry pie lollipop muffin. Carrot cake dragée chupa chups jujubes.
           Macaroon liquorice cookie wafer tart marzipan bonbon. Gingerbread jelly-o dragée chocolate.
